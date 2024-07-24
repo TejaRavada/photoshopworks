f8631dae4cb63f2ff528560f2ea875c7f4331ce0
vtejaravada <ravadateja123@gmail.com>
2024-07-24 15:32:57 +0530

    first commit

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import './nav.css';
 import { IoMdMenu } from "react-icons/io";
-
 
 const Nav = () => {
     const [showNavItems, setShowNavItems] = useState(false);
@@ -11,42 +10,40 @@ const Nav = () => {
         setShowNavItems(!showNavItems);
     };
 
-    const handleClickOutside = (event) => {
+    const handleClickOutside = useCallback((event) => {
         if (showNavItems && !event.target.closest('.nav-mob') && !event.target.closest('.nav-items')) {
             setShowNavItems(false);
         }
-    };
+    }, [showNavItems]);
 
     useEffect(() => {
         document.addEventListener('click', handleClickOutside);
         return () => {
             document.removeEventListener('click', handleClickOutside);
         };
-    }, [showNavItems]);
+    }, [handleClickOutside]);
 
     return (
-        <>
-            <div className="navfixed">
-                <div className="nav-full">
-                    <div className="container">
-                        <div className="nav">
-                            <div className="logo">
-                                <Link to="/">Teja Ravada</Link>
-                            </div>
-                            <div className="nav-mob " onClick={handleToggleMenu}>
-                                <IoMdMenu />
-                            </div>
-                            <div className={`nav-items ${showNavItems ? 'show' : ''}`}>
-                                <Link to="/drawing" onClick={handleToggleMenu}>Drawing</Link>
-                                <Link to="/digital-painting" onClick={handleToggleMenu}>Digital Painting</Link>
-                                <Link to="/background-design" onClick={handleToggleMenu}>Background Design</Link>
-                            </div>
+        <div className="navfixed">
+            <div className="nav-full">
+                <div className="container">
+                    <div className="nav">
+                        <div className="logo">
+                            <Link to="/">Teja Ravada</Link>
+                        </div>
+                        <div className="nav-mob" onClick={handleToggleMenu}>
+                            <IoMdMenu />
+                        </div>
+                        <div className={`nav-items ${showNavItems ? 'show' : ''}`}>
+                            <Link to="/drawing" onClick={handleToggleMenu}>Drawing</Link>
+                            <Link to="/digital-painting" onClick={handleToggleMenu}>Digital Painting</Link>
+                            <Link to="/background-design" onClick={handleToggleMenu}>Background Design</Link>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
-export default Nav
+export default Nav;
