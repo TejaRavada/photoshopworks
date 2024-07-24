e4b5ba949bd573ae3a4be6632af0379412ff53f2
vtejaravada <ravadateja123@gmail.com>
2024-07-24 15:12:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Gallery from './components/Gallery';
+import Nav from './components/navbar/Nav';
+import Footer from './components/footer/Footer.jsx';
+import './responssive.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/drawing" element={<Gallery type="drawing" />} />
+          <Route path="/digital-painting" element={<Gallery type="digitalPainting" />} />
+          <Route path="/background-design" element={<Gallery type="backgroundDesign" />} />
+        </Routes>
+      </div>
+      <Footer/>
+    </Router>
   );
 }
 
